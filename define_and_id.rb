504d13832f245adc94afaf61b9a9f326fb7b33d9
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-01-05 11:34:09 -0700

    Answer method prompts

--- define_and_id.rb
+++ define_and_id.rb
@@ -11,17 +11,30 @@
 
 
 "Hello World".include?("Hello")
-
+# The method include? is called on the string object "Hello World"
+# Include? reviews if a string contains the given argument
+# Within the parameter the "Hello" argument is reviewed
+# The return value is true because the argument "Hello" exists in the given string.
 
 "Hello World".end_with?("Hello")
-
+# The method end_with? is called on the string object "Hello World"
+# end_with? method returns a boolean based on wether the string 
+# ends with the argument in the parameter
+# The return value is false because the string does not end with the argument "Hello"
 
 "Hello World".end_with?("rld")
-
+# The method end_with? is called on the string object "Hello World"
+# end_with? method returns a boolean based on wether the string 
+# ends with the argument in the parameter
+# The return value is true because the string ends with the argument "rld"
 
 12.even?
-
+# The even? method is called on the interger 12
+# This method returns a boolean data type- true if the interger is even
+# The return value is true in this circumstance
 
 18.next
-
+# The next method is called on the interger 18
+# This method returns the interger provided + 1
+# The return value is 19 (18 + 1)
 
